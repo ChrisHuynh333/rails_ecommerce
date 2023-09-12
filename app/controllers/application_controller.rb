2ccd2891771b9f00a1e5552ff369173be0ccae94
Chris Huynh <chris.huynh333@gmail.com>
2023-09-12 15:37:05 -0500

    Add category model, add dropdown to navbar, add font awesome

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    before_action :set_product_categories
+
+    private
+    def set_product_categories
+        @product_categories = ProductCategory.all.sort_by(&:name)
+    end
 end
